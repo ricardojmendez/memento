2fba53d8973e4df66546c73915bb7ad819ddb8c7
Ricardo J. Mendez <ricardo@numergent.com>
2015-07-04 19:52:31 +0300

    Passing the Url for the dev database when running the jar

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -XX:-MaxFDLimit -DPORT=3333 -cp target/memento.jar clojure.main -m memento.core
+java -XX:-MaxFDLimit -Ddatabase.url="jdbc:postgresql://localhost/memento_dev?user=memento&password=testdb" -Dport=3333 -cp target/memento.jar clojure.main -m memento.core
