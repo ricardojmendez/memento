ricardojmendez/memento

commit 57236fea78a041b7b21663fb996d00526871a476

Initial set up for ClojureScript tests

project.clj
@@ -63,25 +63,29 @@
          :uberwar-name "memento.war"}
 
   :migratus {:store         :database
-             :migration-dir "migrations"
-             }
+             :migration-dir "migrations"}
 
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
-  :test-paths ["test/clj" "test/cljc"]
+  :test-paths ["test/clj" "test/cljc" "test/cljs"]
 
   :cljsbuild
-  {:builds
-   {:app
-    {:source-paths ["src/cljs"]
-     :compiler
-                   {:output-dir    "resources/public/js/"
-                    :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
-                    :optimizations :none
-                    :output-to     "resources/public/js/memento.js"
-                    :source-map    "resources/public/js/memento.js.map"
-                    :pretty-print  true}}}}
+  {:builds        {:app  {:compiler
+                                        {:output-dir    "resources/public/js/"
+                                         :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
+                                         :optimizations :none
+                                         :output-to     "resources/public/js/memento.js"
+                                         :source-map    "resources/public/js/memento.js.map"
+                                         :pretty-print  true}}
+                   :test {:compiler
+                                        {:output-dir    "target/test/"
+                                         :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
+                                         :optimizations :whitespace
+                                         :pretty-print  true
+                                         :output-to     "target/test/memento-tests.js"}}
+                   }
+   :test-commands {"test" ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html"]}}
 
 
   :profiles
@@ -135,7 +139,8 @@
                                            :pubkey     "keys/dev_auth_pubkey.pem"
                                            :privkey    "keys/dev_auth_privkey.pem"}
                             }
-             :source-paths ["test/clj" "test/cljc"]
+             :hooks        [leiningen.cljsbuild]
+             :source-paths ["test/clj" "test/cljc" "test/cljs"]
              :cljsbuild    {:builds {:app {:source-paths ["env/dev/cljs"]}}}
              }
    })

phantom/phantomjs-shims.js
@@ -1,0 +1,35 @@
+(function() {
+
+var Ap = Array.prototype;
+var slice = Ap.slice;
+var Fp = Function.prototype;
+
+if (!Fp.bind) {
+  // PhantomJS doesn't support Function.prototype.bind natively, so
+  // polyfill it whenever this module is required.
+  Fp.bind = function(context) {
+    var func = this;
+    var args = slice.call(arguments, 1);
+
+    function bound() {
+      var invokedAsConstructor = func.prototype && (this instanceof func);
+      return func.apply(
+        // Ignore the context parameter when invoking the bound function
+        // as a constructor. Note that this includes not only constructor
+        // invocations using the new keyword but also calls to base class
+        // constructors such as BaseClass.call(this, ...) or super(...).
+        !invokedAsConstructor && context || this,
+        args.concat(slice.call(arguments))
+      );
+    }
+
+    // The bound function must share the .prototype of the unbound
+    // function so that any object created by one constructor will count
+    // as an instance of both constructors.
+    bound.prototype = func.prototype;
+
+    return bound;
+  };
+}
+
+})();

phantom/unit-test.html
@@ -1,0 +1,7 @@
+<html>
+  <body>
+    <script src="phantomjs-shims.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="../target/test/memento-tests.js" type="text/javascript"></script>
+  </body>
+</html>

phantom/unit-test.js
@@ -1,0 +1,38 @@
+var system = require('system');
+
+var page = require('webpage').create();
+var url = system.args[1];// phantom.args[0];
+
+page.onConsoleMessage = function (message) {
+    console.log(message);
+};
+
+function exit(code) {
+    setTimeout(function(){ phantom.exit(code); }, 0);
+    phantom.onError = function(){};
+}
+
+console.log("Loading URL: " + url);
+
+page.open(url, function (status) {
+    if (status != "success") {
+        console.log('Failed to open ' + url);
+        phantom.exit(1);
+    }
+
+    console.log("Running test.");
+
+    var failures = page.evaluate(function() {
+        memento.test.test_runner.runner();
+        return window["test-failures"];
+    });
+
+    if (failures == 0) {
+        console.log("Tests succeeded.")
+    }
+    else {
+        console.log("*** Tests failed! ***");
+    }
+    
+    phantom.exit(failures?100:0);
+});

test/cljs/memento/test/core.cljs
@@ -1,0 +1,13 @@
+(ns memento.test.core
+  (:require [cljs.test :refer-macros [deftest testing is]]
+            [memento.core :as core]))
+
+
+;
+; Trope processor tests
+;
+
+(deftest is-true
+  (println "Hello world")
+  (is (= true true)))
+

test/cljs/memento/test/test_runner.cljs
@@ -1,0 +1,20 @@
+(ns memento.test.test-runner
+  (:require
+    [cljs.test :as test :refer-macros [run-tests] :refer [report]]
+    [memento.test.core]
+    ))
+
+
+(enable-console-print!)
+
+(defmethod report [::test/default :summary] [m]
+  (println "\nRan" (:test m) "tests containing"
+           (+ (:pass m) (:fail m) (:error m)) "assertions.")
+  (println (:fail m) "failures," (:error m) "errors.")
+  (aset js/window "test-failures" (+ (:fail m) (:error m))))
+
+(defn runner []
+  (run-tests
+    (test/empty-env ::test/default)
+    'memento.test.core
+    2585))

commit fa9f4c98bfdf93e06f4db72476f27c558043e380

Fixing cljsbuild regression from 57236fe

project.clj
@@ -69,22 +69,23 @@
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
 
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
-  :test-paths ["test/clj" "test/cljc" "test/cljs"]
+  :test-paths ["test/clj" "test/cljs" "test/cljc"]
 
   :cljsbuild
-  {:builds        {:app  {:compiler
-                                        {:output-dir    "resources/public/js/"
-                                         :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
-                                         :optimizations :none
-                                         :output-to     "resources/public/js/memento.js"
-                                         :source-map    "resources/public/js/memento.js.map"
-                                         :pretty-print  true}}
+  {:builds        {:app  {:source-paths  ["src/cljs"]
+                          :compiler
+                          {:output-dir    "resources/public/js/"
+                           :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
+                           :optimizations :none
+                           :output-to     "resources/public/js/memento.js"
+                           :source-map    "resources/public/js/memento.js.map"
+                           :pretty-print  true}}
                    :test {:compiler
-                                        {:output-dir    "target/test/"
-                                         :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
-                                         :optimizations :whitespace
-                                         :pretty-print  true
-                                         :output-to     "target/test/memento-tests.js"}}
+                          {:output-dir    "target/test/"
+                           :externs       ["react/externs/react.js" "externs/jquery-1.9.js"]
+                           :optimizations :whitespace
+                           :pretty-print  true
+                           :output-to     "target/test/memento-tests.js"}}
                    }
    :test-commands {"test" ["phantomjs" "phantom/unit-test.js" "phantom/unit-test.html"]}}
 