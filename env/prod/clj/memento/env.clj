77597febeb195a9b38e6f4935c89b21fc83882a2
Ricardo J. Mendez <ricardo@numergent.com>
2016-08-01 13:25:56 +0300

    Running migrations on production
    This was a regression from when I updated the handler namespace, did
    not set it to run migrations on production again. This way it's cleaner,
    since we only do it on the production initialization.

--- env/prod/clj/memento/env.clj
+++ env/prod/clj/memento/env.clj
@@ -1,11 +1,17 @@
 (ns memento.env
-    (:require [clojure.tools.logging :as log]))
+  (:require [clojure.tools.logging :as log]
+            [luminus-migrations.core :as migrations]
+            [memento.config :refer [env]]))
 
 (def defaults
   {:init
                (fn []
-                   (log/info "\n-=[memento started successfully]=-"))
+                 (do
+                   (log/info "-=[ Applying migrations ]=-")
+                   (migrations/migrate ["migrate"] (select-keys env [:database-url]))
+                   (log/info "...migrations done")
+                   (log/info "\n-=[memento started successfully]=-")))
    :stop
                (fn []
-                   (log/info "\n-=[memento has shut down successfully]=-"))
+                 (log/info "\n-=[memento has shut down successfully]=-"))
    :middleware identity})
