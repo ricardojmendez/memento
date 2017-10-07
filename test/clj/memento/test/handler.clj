0fa2e5eb08d1fe9d73f097a223413dfb76dec267
Ricardo J. Mendez <ricardo@numergent.com>
2017-10-07 17:09:42 +0200

    Basic tests for main routes

--- test/clj/memento/test/handler.clj
+++ test/clj/memento/test/handler.clj
@@ -4,9 +4,18 @@
             [ring.mock.request :refer :all]))
 
 (deftest test-app
-  (testing "main route"
-    (let [response ((app) (request :get "/"))]
-      (is (= 200 (:status response)))))
+  (testing "Our general routes are setup"
+    (are [status path] (= status (:status ((app) (request :get path))))
+                       200 "/"
+                       200 "/about"
+                       200 "/record"
+                       200 "/login"
+                       200 "/signup"
+                       200 "/remember"
+                       404 "/thread"                        ; Thread expects an ID
+                       200 "/thread/"                       ; ID can be null
+                       200 "/thread/some-id"
+                       ))
 
   (testing "not-found route"
     (let [response ((app) (request :get "/invalid"))]
